9305663d6835ffe711b202432fef76258cfd311a
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2013-06-20 15:05:55 +0000

    First successful reading of a dmap file.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/QtHardMon/trunk@26 5f601a5c-88b9-47f3-b851-818f6741d00d

--- QtHardMon.cpp
+++ QtHardMon.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 
 #include <QMessageBox>
+#include <QFileDialog>
+
+#include <MtcaMappedDevice/exBase.h>
+#include <MtcaMappedDevice/dmapFilesParser.h>
 
 QtHardMon::QtHardMon(QWidget * parent, Qt::WindowFlags flags) 
   : QMainWindow(parent, flags)
@@ -28,13 +32,38 @@ QtHardMon::~QtHardMon()
 
 void  QtHardMon::loadBoards()
 {
-  QMessageBox messageBox;
-  messageBox.setText("Only loading non-existent toy devices.");
-  messageBox.setInformativeText("A file dialog to select the cmap-file will be implemented later.");
-  messageBox.exec();
-  
-  hardMonForm.deviceListWidget->addItem("FirstADCBoard");
-  hardMonForm.deviceListWidget->addItem("SecondADCBoard");
+  //  QMessageBox messageBox;
+
+  dmapFilesParser filesParser;
+
+  QString dmapFileName = QFileDialog::getOpenFileName(this,
+						      tr("Open DeviceMap file"), 
+						      "/home/jana", 
+						      tr("DeviceMap files (*.dmap"));
+  try{
+    filesParser.parse_file(dmapFileName.toStdString());
+  }
+  catch( exBase & e )
+  {
+    QMessageBox messageBox;
+    messageBox.setText("Could not load DeviceMap file "+dmapFileName+".");
+    messageBox.setInformativeText(QString("Info: An exception was thrown:")+e.what());
+    messageBox.exec();
+    
+    // We just return after displaying the message and leave the deviceList as it was.
+    return;
+  }
+
+  hardMonForm.deviceListWidget->clear();
+
+  for (dmapFilesParser::iterator deviceIter = filesParser.begin();
+       deviceIter != filesParser.end(); ++deviceIter)
+  {
+    
+    // fixme: Issue in libdevMap: why is the iterator on a pair?
+    hardMonForm.deviceListWidget->addItem((*deviceIter).first.dev_name.c_str());
+  }
+
 }
 
 void QtHardMon::deviceSelected(QListWidgetItem * deviceItem)
