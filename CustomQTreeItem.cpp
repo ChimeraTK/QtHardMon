57341544e749a1429834d2033547d6487321a252
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-07-14 16:07:59 +0000

    introduced RegisterEntry
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/QtHardMon/branches/test_branch@1189 5f601a5c-88b9-47f3-b851-818f6741d00d

--- CustomQTreeItem.cpp
+++ CustomQTreeItem.cpp
@@ -11,7 +11,11 @@ CustomQTreeItem::CustomQTreeItem(QTreeWidget* parent_, const QString& text_,
                                  const int type_)
     : QTreeWidgetItem(parent_, QStringList(text_), type_) {}
 
-mtca4u::mapFile::mapElem CustomQTreeItem::getRegisterMapElement() {
+CustomQTreeItem::CustomQTreeItem(QTreeWidgetItem* parent_, const QString& text_,
+                                 const int type_)
+    : QTreeWidgetItem(parent_, QStringList(text_), type_) {}
+
+mtca4u::mapFile::mapElem const CustomQTreeItem::getRegisterMapElement() {
   return (mtca4u::mapFile::mapElem());
 }
 
@@ -19,10 +23,24 @@ CustomQTreeItem::~CustomQTreeItem() {
   // TODO Auto-generated destructor stub
 }
 
-ModuleEntry::ModuleEntry(QTreeWidget* parent_, const QString& text_): CustomQTreeItem(parent_, text_, ModuleEntry::DataType) {}
+ModuleEntry::ModuleEntry(QTreeWidget* parent_, const QString& text_)
+    : CustomQTreeItem(parent_, text_, ModuleEntry::DataType) {}
 
-void ModuleEntry::read() {}
+void ModuleEntry::read(QTableWidget* const tablewidget) {}
 
-void ModuleEntry::write() {}
+void ModuleEntry::write(QTableWidget* const tablewidget) {}
 
-void ModuleEntry::updateRegisterProperties() {}
+void ModuleEntry::updateRegisterProperties(
+    const RegsterPropertyGrpBox& grpBox) {}
+
+RegisterEntry::RegisterEntry(mtca4u::mapFile::mapElem registerInfo,
+                             QTreeWidgetItem* parent_, const QString& text_)
+    : CustomQTreeItem(parent_, text_, RegisterEntry::DataType),
+      _registerMapElement(registerInfo) {}
+
+void RegisterEntry::read(QTableWidget* const tablewidget) {}
+
+void RegisterEntry::write(QTableWidget* const tablewidget) {}
+
+void RegisterEntry::updateRegisterProperties(
+    const RegsterPropertyGrpBox& grpBox) {}
--- CustomQTreeItem.h
+++ CustomQTreeItem.h
@@ -10,6 +10,31 @@
 
 #include <MtcaMappedDevice/mapFile.h>
 #include <qtreewidget.h>
+#include <qlabel.h>
+#include <qtablewidget.h>
+
+struct RegsterPropertyGrpBox {
+  QLabel* registerNameDisplay;
+  QLabel* moduleDisplay;
+  QLabel* registerBarDisplay;
+  QLabel* registerAddressDisplay;
+  QLabel* registerNElementsDisplay;
+  QLabel* registerSizeDisplay;
+  QLabel* registerWidthDisplay;
+  QLabel* registerFracBitsDisplay;
+  QLabel* registeSignBitDisplay;
+
+  RegsterPropertyGrpBox()
+      : registerNameDisplay(0),
+        moduleDisplay(0),
+        registerBarDisplay(0),
+        registerAddressDisplay(0),
+        registerNElementsDisplay(0),
+        registerSizeDisplay(0),
+        registerWidthDisplay(0),
+        registerFracBitsDisplay(0),
+        registeSignBitDisplay(0) {}
+};
 
 /**
  * interface class for Qtree custom items
@@ -19,38 +44,56 @@ public:
   /**
    * Default constructor
    */
-  CustomQTreeItem(QTreeWidget* parent_, const QString& text_, const int type);
+  CustomQTreeItem(QTreeWidget* parent_, const QString& text_, const int type_);
+  CustomQTreeItem(QTreeWidgetItem* parent_, const QString& text_,
+                  const int type_);
   /**
    * read in from the card and polpulate the table widget
    */
-  virtual void read() = 0;
+  virtual void read(QTableWidget* const tablewidget) = 0;
   /**
    * write user updated values from the table to the card
    */
-  virtual void write() = 0;
+  virtual void write(QTableWidget* const tablewidget) = 0;
 
   /**
    * update the register properties window of qthardmon
    */
-  virtual void updateRegisterProperties() = 0;
+  virtual void updateRegisterProperties(
+      RegsterPropertyGrpBox const& grpBox) = 0;
 
   /**
    * retuen the map file element if applicable
    */
-  virtual mtca4u::mapFile::mapElem getRegisterMapElement();
+  virtual mtca4u::mapFile::mapElem const getRegisterMapElement();
   /**
    *
    */
   virtual ~CustomQTreeItem();
 };
 
+
 class ModuleEntry : public CustomQTreeItem {
 public:
   ModuleEntry(QTreeWidget* parent_, const QString& text_);
-  virtual void read();
-  virtual void write();
-  virtual void updateRegisterProperties();
+  virtual void read(QTableWidget* const tablewidget);
+  virtual void write(QTableWidget* const tablewidget);
+  virtual void updateRegisterProperties(RegsterPropertyGrpBox const& grpBox);
+
   static const int DataType = QTreeWidgetItem::UserType + 1;
 };
 
+class RegisterEntry : public CustomQTreeItem {
+public:
+  RegisterEntry(mtca4u::mapFile::mapElem registerInfo, QTreeWidgetItem* parent_,
+                const QString& text_);
+  virtual void read(QTableWidget* const tablewidget);
+  virtual void write(QTableWidget* const tablewidget);
+  virtual void updateRegisterProperties(RegsterPropertyGrpBox const& grpBox);
+
+  static const int DataType = QTreeWidgetItem::UserType + 2;
+
+private:
+  mtca4u::mapFile::mapElem _registerMapElement;
+};
 #endif /* SOURCE_DIRECTORY__CUSTOMQTREEITEM_H_ */
