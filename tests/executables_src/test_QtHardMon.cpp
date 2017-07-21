81dd5874cc4c34893d68ea44ebf2d94b6a087053
Filip Makowski <filip.makowski@desy.de>
2017-07-21 12:35:23 +0200

    Added map files and specified test case based on them.

--- tests/executables_src/test_QtHardMon.cpp
+++ tests/executables_src/test_QtHardMon.cpp
@@ -28,16 +28,25 @@ struct QtHardmon_populatesRegisterTree_fixture {
 BOOST_AUTO_TEST_CASE ( QtHardMon_populatesRegisterTree )
 {
 
-    QtHardmon_populatesRegisterTree_fixture fixtureNumerical("test_QtHardMon_populatesRegisterTree.dmap", "NumDev");
+    QtHardmon_populatesRegisterTree_fixture fixtureNumerical("test_QtHardMon_populatesRegisterTree.dmap", "NUMDEV");
 
-    BOOST_CHECK_EQUAL(fixtureNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItemCount(), 2);
-    BOOST_CHECK_EQUAL(fixtureNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(0)->childCount(), 3);
-    BOOST_CHECK_EQUAL(fixtureNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(1)->childCount(), 2);
+    // Expecting BOARD, APP0, MODULE0 and MODULE1 modules
+    BOOST_CHECK_EQUAL(fixtureNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItemCount(), 4);
+    // BOARD has 2 items
+    BOOST_CHECK_EQUAL(fixtureNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(0)->childCount(), 2);
+    // APP0 has 4 items
+    BOOST_CHECK_EQUAL(fixtureNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(1)->childCount(), 4);
+    // MODULE0 has 2 items
+    BOOST_CHECK_EQUAL(fixtureNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(2)->childCount(), 2);
+    // MODULE1 has 3 items
+    BOOST_CHECK_EQUAL(fixtureNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(3)->childCount(), 3);
 
-    QtHardmon_populatesRegisterTree_fixture fixtureNonNumerical("test_QtHardMon_populatesRegisterTree.dmap", "LmapDev");
+    QtHardmon_populatesRegisterTree_fixture fixtureNonNumerical("test_QtHardMon_populatesRegisterTree.dmap", "LMAPDEV");
 
-    BOOST_CHECK_EQUAL(fixtureNonNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItemCount(), 3);
-    BOOST_CHECK_EQUAL(fixtureNonNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(0)->childCount(), 2);
-    BOOST_CHECK_EQUAL(fixtureNonNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(1)->childCount(), 1);
+    // Expecting MyModule and module, that contains all uncategorized registers
+    BOOST_CHECK_EQUAL(fixtureNonNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItemCount(), 2);
+    // Uncategorized has 5 items: 3 registers and 2 constants
+    BOOST_CHECK_EQUAL(fixtureNonNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(0)->childCount(), 5);
+    // MyModule has two items: 1 register and 1 submodule
     BOOST_CHECK_EQUAL(fixtureNonNumerical.qtHardMon._hardMonForm.registerTreeWidget->topLevelItem(2)->childCount(), 2);
 }
