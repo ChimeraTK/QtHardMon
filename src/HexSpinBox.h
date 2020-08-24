8cccbadb2fe0fb9d8256b90b9b976a480aae7984
Martin Killenberg <martin.killenberg@desy.de>
2020-08-24 17:27:04 +0200

    fixed HexSpinBox, which was broken by previous commit

--- src/HexSpinBox.h
+++ src/HexSpinBox.h
@@ -21,7 +21,7 @@ class HexData {
    * Holds the entered value (for which we need the hex representation)
    */
   double value;
-  HexData(double = 0) : value() {}
+  HexData(double v = 0) : value(v){};
   /** Overloaded constructor for std::string to avoid template specialisation as
    *  there is no implicit conversion from std::string to double.
    *  Generally this conversion does not make sense, so we initialise class with
